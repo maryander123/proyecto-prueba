a10c01c02542dd460084d642dd67b0c723b8c102
maryander123 <maryandert14@gmail.com>
2024-08-15 07:34:47 +0200

    fixed link prod

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const PAGE_URL = process.env.NODE_ENV === 'production'
-? 'placeholder'
+? 'https://baile-proyect.onrender.com'
 : 'http://localhost:3003';
 
 const MONGO_URI = process.env.NODE_ENV === 'production'
